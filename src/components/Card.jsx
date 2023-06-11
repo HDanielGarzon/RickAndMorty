15e1686a4db4d040c0da3f1f8419ca4930a066ac
Daniel <harold.daniel94@hotmail.com>
2023-06-10 22:31:06 -0500

    creacion de repositorio rick and morty

---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+export default function Card({id, name, status, species, gender, origin, image, onClose}) {
+   return (
+      <div>
+         <button onClick={onClose}>X</button> 
+         <h2>{name}</h2>
+         <h2>{status}</h2>
+         <h2>{species}</h2>
+         <h2>{gender}</h2>
+         <h2>{origin}</h2>
+         <img src={image} alt='' /> 
+      </div>
+   );
+}
---
+++ src/components/Cards.jsx
@@ -0,0 +1,23 @@
+import Card from './Card';
+
+export default function Cards({characters}) {
+   console.log('characters:::::::',characters);
+   return(
+   <div>
+      {
+       characters.map(({id, name, status, species, gender, origin, image})=>{
+           return( <Card key={id} 
+            id={id}
+            name={name} 
+            status={status} 
+            species={species} 
+            gender={gender} 
+            origin={origin.name} 
+            image={image}
+            onClose={() => window.alert('Emulamos que se cierra la card')} />);
+         })
+      }
+      
+   </div>
+   )
+}
